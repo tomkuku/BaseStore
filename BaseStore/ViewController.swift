46e6a2080d3561fb6305ccd4cf2d11018af9c786
Tomasz Kukułka <tomasz.kukulka@ailleron.com>
2022-02-22 10:47:22 +0100

    Initial Commit

---
+++ BaseStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseStore
+//
+//  Created by Tomasz Kukułka on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
