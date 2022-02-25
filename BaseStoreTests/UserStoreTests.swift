d99673b01e5007b62a30cab57c814c96c00541ef
Tomasz Kukułka <tomasz.kukulka@ailleron.com>
2022-02-25 11:38:18 +0100

    Deleted UserDefaultsMock

--- BaseStoreTests/UserStoreTests.swift
+++ BaseStoreTests/UserStoreTests.swift
@@ -20,32 +20,17 @@ private enum UserStoreTestKey: BaseStoreKey {
     }
 }
 
-private final class UserDefaultsMock: UserDefaults {
-    
-    private var store: [String: Any?] = [:]
-    
-    override func set(_ value: Any?, forKey defaultName: String) {
-        store[defaultName] = value
-    }
-    
-    override func object(forKey defaultName: String) -> Any? {
-        return store[defaultName] as? Any
-    }
-    
-    override func removeObject(forKey defaultName: String) {
-        store.removeValue(forKey: defaultName)
-    }
-}
-
 class UserStoreTests: XCTestCase {
     
-    private var mock: UserDefaultsMock!
+    private var mock: UserDefaults!
     private var sut: UserStore<UserStoreTestKey>!
     
     override func setUp() {
         super.setUp()
         
-        mock = UserDefaultsMock()
+        let suiteName = "user-store-test-suite"
+        mock = UserDefaults(suiteName: suiteName)
+        mock.removePersistentDomain(forName: suiteName)
         sut = UserStore(userDefaults: mock)
     }
     
@@ -58,41 +43,53 @@ class UserStoreTests: XCTestCase {
     
     // MARK: Tests
     
-    func test_settingAndRecievingValue() {
+    func test_setValue() {
         let value: Int = 71
         
         sut.set(value: value, forKey: .testValue)
-        
-        let recievedValue: Int? = sut.recieve(forKey: .testValue)
-        
-        assertThat(value == recievedValue)
     }
     
+    func test_receivingNotSetValue() {
+        let recievedValue: Int? = sut.recieve(forKey: .testValue)
+        
+        assertThat(recievedValue, equalTo(nil))
+    }
+    
+    func test_recievingSetValue() {
+        let value: Int = 71
+
+        sut.set(value: value, forKey: .testValue)
+
+        let recievedValue: Int? = sut.recieve(forKey: .testValue)
+
+        assertThat(value == recievedValue)
+    }
+
     func test_updatingAndRecievingValue() {
         var value: Int = 45
-        
+
         sut.set(value: value, forKey: .testValue)
-        
+
         var recievedValue: Int? = sut.recieve(forKey: .testValue)
-        
+
         value = 99
-        
+
         sut.set(value: value, forKey: .testValue)
-        
+
         recievedValue = sut.recieve(forKey: .testValue)
-        
+
         assertThat(value == recievedValue)
     }
-    
+
     func test_removingValue() {
         let piValue: Int = 88
-        
+
         sut.set(value: piValue, forKey: .testValue)
-        
+
         sut.remove(forKey: .testValue)
-        
+
         let recievedValue: Int? = sut.recieve(forKey: .testValue)
-        
+
         assertThat(recievedValue, nilValue())
     }
 }
